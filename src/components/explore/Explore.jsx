450be4625f46b807c1fe195c6135de2102e85fe0
vladie777 <diego.800luis@gmail.com>
2023-05-22 20:17:09 -0500

    cambios al home

--- src/components/explore/Explore.jsx
+++ src/components/explore/Explore.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import './styleExplore.scss'
-
 const Explore = () => {
   return (
     <div className="Container">
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,7 +3,6 @@ import TextSpace from "../textSpace/TextSpace";
 import "./styleHome.scss";
 import Explore from "../explore/Explore";
 import Layout from "../layout/Layout";
-
 const Home = () => {
   return (
     <div>
